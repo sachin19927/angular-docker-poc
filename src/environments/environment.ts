e6524ef58d2a12c3e1f3bd427f5c50d01fd02571
sachin19927 <sachin19927@gmail.com>
2024-07-31 19:18:52 +0530

    docker images through pack cli

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+import {version,angularVersion} from './version';
+
+const GATEWAY_BASE_URL='/api/';
+const CLIENT_BASE_URL='';
+const SESSION_WARN_DISPLAY_TIME=120;
+const SESSION_WARN_EXPIRY_TIME=30;
+
+export const environment = {
+  production: false,
+  version:version,
+  angularVersion: angularVersion,
+  appName: 'Web App',
+  gatewayUrl: GATEWAY_BASE_URL,
+  clientUrl: CLIENT_BASE_URL,
+  sessionWarnDisplay: SESSION_WARN_DISPLAY_TIME,
+  sessionWarnExpire: SESSION_WARN_EXPIRY_TIME
+};
